8c59919b5a11fcecbdbdbb2de5a37ef981c19732
Georgi Popov <magadanski.uchen@gmail.com>
2013-08-04 17:01:08 +0300

    notice fixes -- front-end styles and scripts enqueued on wp_enqueue_scripts

--- lib/Censeo_Options.php
+++ lib/Censeo_Options.php
@@ -1,48 +1,6 @@
 <?php
-class Censeo_Options {
-	protected $id;
-	protected $parent = false;
-	
-	public $title;
-	public $capability;
-	public $icon = '';
-	public $position = 90;
-	
-	public function __construct($id, $title, $capability='administrator', $parent=false) {
-		$this->id = sanitize_title_with_dashes($id);
-		$this->title = $title;
-		$this->capability = $capability;
-		$this->parent = $parent;
-		
-		add_action('admin_menu', array(&$this, 'init'));
-	}
-	
-	public function get_id() {
-		return $this->id;
-	}
-	
-	public function get_parent() {
-		return $this->parent;
-	}
-	
-	public function init() {
-		$page_title = $this->title;
-		$menu_title = $this->title;
-		$capability = $this->capability;
-		$menu_slug = $this->id;
-		$function = array(&$this, 'render');
-		$icon_url = esc_url($this->icon);
-		$position = absint($this->position);
-		
-		if ($this->parent) {
-			add_submenu_page($this->parent, $page_title, $menu_title, $capability, $menu_slug, $function);
-		} else {
-			add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position);
-		}
-	}
-	
-	public function render() {
-		
-	}
+require_once(CENSEO_LIB . 'Censeo_Page.php');
+
+class Censeo_Options extends Censeo_Page {
 }
 ?>
