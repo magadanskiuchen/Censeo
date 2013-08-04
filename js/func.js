e3d0051f2184777b792cb326a3c5c1b3e3e3099a
Georgi Popov <magadanski.uchen@gmail.com>
2013-08-04 16:18:48 +0300

    added js fallback for placeholder HTML attribute

--- js/func.js
+++ js/func.js
@@ -1,3 +1,29 @@
-jQuery(function ($) {
+var support = {};
+
+(function () {
+	var i = document.createElement('input');
 	
+	support.placeholder = 'placeholder' in i;
+})();
+
+jQuery(function ($) {
+	if (!support.placeholder) {
+		$('input[placeholder]').each(function () {
+			$this = $(this);
+			
+			$this.focus(function () {
+				if ($this.val() == $this.attr('placeholder')) {
+					$this.val('');
+				}
+			}).blur(function () {
+				if ($this.val() == '') {
+					$this.val($this.attr('placeholder'));
+				}
+			});
+			
+			if ($this.val() == '') {
+				$this.val($this.attr('placeholder'));
+			}
+		});
+	}
 });
