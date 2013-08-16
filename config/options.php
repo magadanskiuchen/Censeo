65ec78bda9f4a88e3a2da6b14e1951b05bd2571e
Georgi Popov <magadanski.uchen@gmail.com>
2013-08-16 22:14:50 +0300

    added options page with a test field

--- config/options.php
+++ config/options.php
@@ -3,4 +3,20 @@
 Sample usage:
 $options_page = new Censeo_Options($id, $title, $capabilities='administrator', $parent=false);
 */
+
+/////////////////////////////////
+// Options Page//////////////////
+/////////////////////////////////
+$options_page = new Censeo_Options('censeo-options', __('Censeo Options', 'censeo'));
+
+$test_option = new Censeo_Field('test', __('Test', 'censeo'));
+
+$options_page->add_fields(array($test_option));
+
+
+
+/////////////////////////////////
+// Social Options Page //////////
+/////////////////////////////////
+$social_options_page = new Censeo_Options('censeo-social-options', __('Social Options', 'censeo'), 'administrator', 'censeo-options');
 ?>
