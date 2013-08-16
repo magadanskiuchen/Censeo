7726f08933e08ab429eed012d9b34a43534b8cd0
Georgi Popov <magadanski.uchen@gmail.com>
2013-08-16 21:30:26 +0300

    WIP on Censeo_Options and Censeo_Field

--- lib/Censeo_Fields.php
+++ lib/Censeo_Fields.php
@@ -12,5 +12,71 @@
  * @since 0.1
  */
 class Censeo_Field {
+	protected $name;
+	protected $value = '';
+	protected $classes = array('censeo-field');
+	protected $label;
+	
+	public function __construct($name, $value, $label) {
+		$this->name = $name;
+		$this->set_value($value);
+		$this->set_label($label);
+	}
+	
+	public function get_name() {
+		return $this->name;
+	}
+	
+	public function get_value() {
+		return $this->value;
+	}
+	
+	public function set_value($value) {
+		$this->value = apply_filters('censeo_validate_value', array($this, 'validate'), $value);
+	}
+	
+	public function load_value($value) {
+		if (isset($_POST[$this->name])) {
+			$this->set_value($_POST[$this->name]);
+		}
+	}
+	
+	public function get_label() {
+		return $this->label;
+	}
+	
+	public function set_label($label) {
+		$this->label = $label;
+	}
+	
+	public function get_classes() {
+		return $this->classes;
+	}
+	
+	public function add_class($class) {
+		$this->classes = array_unique($classes[] = $class);
+		
+		return $this->classes;
+	}
+	
+	public function remove_class($class) {
+		if ($idx = array_search($class, $this->classes)) {
+			unset($this->classes[$idx]);
+		}
+		
+		return $this->classes;
+	}
+	
+	public function validate($value) {
+		return $value;
+	}
+	
+	protected function render_field() {
+		return '<input id="' . esc_attr($this->get_name()) . '" name="' . esc_attr($this->get_name()) . '" class="' . esc_attr(implode(' ', $this->get_classes())) . '" value="' . esc_attr($this->get_value()) . '" />';
+	}
+	
+	public function render() {
+		return '<div class="censeo-field"><label for="' . esc_attr($this->get_name()) . '">' . esc_html($this->label) . '</label>' . $this->render_field() . '</div>';
+	}
 }
 ?>
--- lib/Censeo_Options.php
+++ lib/Censeo_Options.php
@@ -16,5 +16,22 @@ require_once(CENSEO_LIB . 'Censeo_Page.php');
  * @since 0.1
  */
 class Censeo_Options extends Censeo_Page {
+	protected $fields = array();
+	
+	public function add_field(Censeo_Field $field) {
+		$this->fields[] = $field;
+	}
+	
+	public function render() {
+		parent::render();
+		do_action('censeo_options_' . $this->get_id() . '_before_render');
+		
+		echo '<h2>' . $this->title . '</h2>';
+		foreach ($this->fields as $field) {
+			$field->render();
+		}
+		
+		do_action('censeo_options_' . $this->get_id() . '_after_render');
+	}
 }
 ?>
