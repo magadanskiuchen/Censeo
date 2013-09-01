40a2ba9aa45223ee55ae307899d580ea8fea6486
Georgi Popov <magadanski.uchen@gmail.com>
2013-09-01 21:19:11 +0300

    added testing options configuration

--- config/options.php
+++ config/options.php
@@ -11,7 +11,15 @@ $options_page = new Censeo_Options('censeo-options', __('Censeo Options', 'cense
 
 $test_option = new Censeo_Field('test', __('Test', 'censeo'));
 
-$options_page->add_fields(array($test_option));
+$test_number = new Censeo_Field_Number('test_number', __('Number', 'censeo'));
+$test_number->set_min(0);
+$test_number->set_max(10);
+$test_number->set_step(2);
+
+$options_page->add_fields(array(
+	$test_option,
+	$test_number,
+));
 
 
 
