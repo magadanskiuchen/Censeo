d8797bc6066e092655c31f0590d77a83313557ac
Georgi Popov <magadanski.uchen@gmail.com>
2014-06-28 15:55:27 +0300

    post_meta configuration updates

--- config/post_meta.php
+++ config/post_meta.php
@@ -4,4 +4,11 @@ Sample usage:
 $meta_box = new Censeo_Pots_Meta($id, $title, $post_type = 'post');
 */
 
+$post_meta = new Censeo_Post_Meta('post_meta', __('Post Options', 'censeo'), 'post');
+
+$test_field = new Censeo_Field('test', __('Test', 'censeo'));
+
+$post_meta->set_fields(array(
+	$test_field,
+));
 ?>
